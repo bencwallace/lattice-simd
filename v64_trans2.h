10888844be264d99ea77ae54ae343135f27ffd96
Ben Wallace <bencwallace@gmail.com>
2024-12-22 13:12:47 -0500

    use better circular shift

--- v64_trans2.h
+++ v64_trans2.h
@@ -4,8 +4,23 @@
 #include <bit>
 #include <cstdint>
 #include <immintrin.h>
+#include <limits.h>
 
 #include "v64_point2.h"
+
+// https://stackoverflow.com/questions/776508/best-practices-for-circular-shift-rotate-operations-in-c
+uint64_t rotl (uint64_t x, unsigned int n)
+{
+  const unsigned int mask = (CHAR_BIT*sizeof(x)-1);  // e.g. 31
+
+  n &= mask;  // avoid undef behaviour with NDEBUG.  0 overhead for most types / compilers
+  return (x<<n) | (x>>( (-n)&mask ));
+}
+
+uint64_t rot_const(uint64_t x)
+{
+  return rotl(x, 32);
+}
 
 struct v64_trans2 {
     __m64 signs;
@@ -17,7 +32,7 @@ struct v64_trans2 {
         {}
 
     v64_point2 operator*(const v64_point2 &p) const {
-        uint64_t temp = std::rotl(p.data, static_cast<int32_t>(perm));
+        uint64_t temp = rot_const(p.data);
         auto temp2 = _mm_sign_pi32(_mm_cvtsi64_m64(temp), signs);
         return _mm_cvtm64_si64(temp2);
     }
