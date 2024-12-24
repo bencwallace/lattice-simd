e7927fef2b5450c910542dd830f7d1731e3da502
Ben Wallace <bencwallace@gmail.com>
2024-12-23 20:06:09 -0500

    WIP: pack 16-bit coordinates into 32 bit vector

---
+++ src/v32_trans2d16.h
@@ -0,0 +1,25 @@
+#include <cstddef>
+#include <cstdint>
+
+struct v32_point2d16 {
+  uint32_t data;
+
+  v32_point2d16(int16_t x, int16_t y)
+      : data((uint32_t(y) << 16) | uint16_t(x)) {}
+
+  int16_t operator[](size_t i) const { return (data >> (16 * i)) & 0xFFFF; }
+};
+
+// Idea:
+//  xor eax, <mask>
+//  add ax, <lower>
+//  add eax, <upper>
+// where mask = 0xFFFFFFFF or 0xFFFF0000 or 0x0000FFFF,
+// lower = 0 or 1, and upper = 0 or 0x10000
+
+struct v32_trans2d16 {
+  v32_point2d16 operator*(const v32_point2d16 p) const {
+    // TODO
+    return v32_point2d16(p[1], p[0]);
+  }
+};
