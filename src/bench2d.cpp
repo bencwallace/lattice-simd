9777bc8b09db318882cdbd19fa8c44ffc5c8e0f1
Ben Wallace <bencwallace@gmail.com>
2024-12-24 16:49:15 -0500

    fix 2x32-bit rotation and add test cases

--- src/bench2d.cpp
+++ src/bench2d.cpp
@@ -4,41 +4,53 @@
 #include "v64_trans2d.h"
 #include "v128_trans2d.h"
 
+
+template <bool OptimizeT>
 static void BM_STransform(benchmark::State& state) {
     s_point<2> p = {1, 2};
     s_trans<2> t = {{-1, -1}, {1, 0}};
     s_point<2> q;
-    benchmark::DoNotOptimize(t);
+    if constexpr (!OptimizeT) {
+        benchmark::DoNotOptimize(t);
+    }
     for (auto _: state) {
         q = t * p;
         benchmark::DoNotOptimize(q);
     }
 }
-BENCHMARK(BM_STransform);
+BENCHMARK_TEMPLATE(BM_STransform, false);
+BENCHMARK_TEMPLATE(BM_STransform, true);
 
+template <bool OptimizeT>
 static void BM_V128Transform(benchmark::State& state) {
     v128_point2 p = {1, 2};
     v128_trans2 t = {{-1, 1}, {1, 0}};
     v128_point2 q;
-    benchmark::DoNotOptimize(t);
+    if constexpr (!OptimizeT) {
+        benchmark::DoNotOptimize(t);
+    }
     for (auto _: state) {
         q = t * p;
         benchmark::DoNotOptimize(q);
     }
 }
-BENCHMARK(BM_V128Transform);
+BENCHMARK_TEMPLATE(BM_V128Transform, false);
+BENCHMARK_TEMPLATE(BM_V128Transform, true);
 
-
+template <bool OptimizeT>
 static void BM_V64Transform(benchmark::State& state) {
     v64_point2 p = {1, 2};
     v64_trans2 t = {{-1, 1}, true};
     v64_point2 q;
-    benchmark::DoNotOptimize(t);
+    if constexpr (!OptimizeT) {
+        benchmark::DoNotOptimize(t);
+    }
     for (auto _: state) {
         q = t * p;
         benchmark::DoNotOptimize(q);
     }
 }
-BENCHMARK(BM_V64Transform);
+BENCHMARK_TEMPLATE(BM_V64Transform, false);
+BENCHMARK_TEMPLATE(BM_V64Transform, true);
 
 BENCHMARK_MAIN();
--- src/v64_trans2d.h
+++ src/v64_trans2d.h
@@ -8,18 +8,17 @@
 
 #include "v64_point2d.h"
 
-// Would be nice to have a more portable version of this using ideas from here:
 // https://stackoverflow.com/questions/776508/best-practices-for-circular-shift-rotate-operations-in-c
-// However, attempts so far have been less performant than the inline assembly version.
-// For some reason, this is even the case when calling __rolq(p.data, iperm) from <x86intrin.h>.
-static inline uint64_t rol64_fixed(uint64_t value) {
-    __asm__("rolq $32, %0" : "+r" (value));
-    return value;
+static inline uint64_t rol64(uint64_t n, uint8_t c)
+{
+  const unsigned int mask = (CHAR_BIT*sizeof(n) - 1);
+  c &= mask;
+  return (n<<c) | (n>>( (-c)&mask ));
 }
 
 struct v64_trans2 {
     __m64 signs;
-    uint32_t iperm;
+    uint8_t iperm;
 
     v64_trans2(std::array<int32_t, 2> signs, bool perm)
         : signs(_mm_setr_pi32(signs[0], signs[1])),
@@ -28,7 +27,7 @@ struct v64_trans2 {
 
     // T(p)[i] = S[i] * p[Pinv[i]]
     v64_point2 operator*(const v64_point2 &p) const {
-        uint64_t temp = rol64_fixed(p.data);
+        uint64_t temp = rol64(p.data, iperm);
         auto temp2 = _mm_sign_pi32(_mm_cvtsi64_m64(temp), signs);
         return _mm_cvtm64_si64(temp2);
     }
